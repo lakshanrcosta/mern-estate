4ad0e1681550ed8829aaf648f371e6f5a2312203
Lakshan Costa <lakshanrcosta@gmail.com>
2023-11-18 02:05:37 +0200

    Add main routes and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home.page';
+import SignIn from './pages/signIn.page';
+import About from './pages/about.page';
+import Profile from './pages/profile.page';
+import SignOut from './pages/sign-out.page';
+import Header from './components/Header/header.component';
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-out" element={<SignOut />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- client/src/pages/sign-out.page.jsx
+++ client/src/pages/sign-out.page.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const SignOut = () => {
   return <div>SignOut</div>;
 };

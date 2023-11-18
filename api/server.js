d88b719d1919ecb706c436a5b9123664c03c5f6e
Lakshan Costa <lakshanrcosta@gmail.com>
2023-11-18 03:52:53 +0200

    Created express app and created mongoose db connection

--- api/server.js
+++ api/server.js
@@ -1,4 +1,10 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { createDatabaseConnection } from './db/dbConnect.js';
+
+dotenv.config();
+
+createDatabaseConnection();
 
 const app = express();
 

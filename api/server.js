c68d6b75e0f1ab225e6c630cd5caa7bb6e5a11e1
Lakshan Costa <lakshanrcosta@gmail.com>
2023-11-18 04:38:35 +0200

    Added test api route

--- api/server.js
+++ api/server.js
@@ -2,11 +2,16 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { createDatabaseConnection } from './db/dbConnect.js';
 
+// Routes
+import userRouter from './routes/user.route.js';
+
 dotenv.config();
 
 createDatabaseConnection();
 
 const app = express();
+
+app.use('/api/user', userRouter);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');

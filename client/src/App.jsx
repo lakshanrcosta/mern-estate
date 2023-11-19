880204a2668c0d72d6267dd5cc3a3da02e005574
Lakshan Costa <lakshanrcosta@gmail.com>
2023-11-19 04:13:15 +0200

    Created sign up page layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home.page';
 import SignIn from './pages/signIn.page';
+import { SignUp } from './pages/signup.page';
 import About from './pages/about.page';
 import Profile from './pages/profile.page';
 import SignOut from './pages/sign-out.page';
@@ -13,6 +14,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/sign-out" element={<SignOut />} />
